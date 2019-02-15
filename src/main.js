36d229e892c659dd2cfb381207d27ebd68f3246c
Rémi GALICHON <Remi.GALICHON@Equadex.net>
2019-02-15 13:27:24 +0100

    init router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import App from './app/App.vue';
 import store from './app/store';
+import router from './app/router';
 
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount('#app');
